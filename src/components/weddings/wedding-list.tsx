8231bf8b5a71da6a3e523834238776fa487a1652
Mike Ling <mikeling.dev@gmail.com>
2025-04-13 20:38:55 +0800

    added delete button for wedding lists

--- src/components/weddings/wedding-list.tsx
+++ src/components/weddings/wedding-list.tsx
@@ -4,10 +4,24 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { format } from "date-fns";
-import { Calendar, ClipboardList, FerrisWheel, Sparkles } from "lucide-react";
+import {
+  Calendar,
+  ClipboardList,
+  FerrisWheel,
+  Sparkles,
+  Trash,
+} from "lucide-react";
 import { useWeddings } from "@/lib/hooks/useWeddings";
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/lib/hooks/useAuth";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
 
 export function WeddingList() {
   const { weddings, loading, error } = useWeddings();
@@ -56,29 +70,51 @@ export function WeddingList() {
   return (
     <div className="w-full py-8 px-6 md:px-12">
       <h2 className="text-2xl font-semibold mb-4">Your Weddings</h2>
-      <div className="space-y-4">
+      <div className="gap-4 grid md:grid-cols-2 lg:grid-cols-3">
         {weddings.map((wedding) => (
-          <Card key={wedding.id}>
-            <CardContent className="py-6">
-              <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
-                <div className="space-y-2">
-                  <h3 className="text-xl font-medium">
-                    {wedding.partner1Name} & {wedding.partner2Name}
-                  </h3>
-                  <div className="flex items-center gap-4 text-sm text-muted-foreground">
-                    <div className="flex items-center gap-1">
-                      <Calendar className="h-4 w-4" />
-                      {format(new Date(wedding.date), "PPP")}
-                    </div>
-                    <div className="flex items-center gap-1">
-                      <FerrisWheel className="h-4 w-4" />
-                      {wedding.theme}
+          <Card key={wedding.id} className="w-full">
+            <CardContent className="py-4">
+              <div className="flex flex-col justify-between items-start gap-4">
+                <div className="flex flex-row justify-between w-full">
+                  <div className="space-y-2">
+                    <h3 className="text-xl font-medium">
+                      {wedding.partner1Name} & {wedding.partner2Name}
+                    </h3>
+                    <div className="flex flex-col gap-1 text-sm text-muted-foreground">
+                      <div className="flex items-center gap-1">
+                        <Calendar className="h-4 w-4" />
+                        {format(new Date(wedding.date), "PPP")}
+                      </div>
+                      <div className="flex items-center gap-1">
+                        <FerrisWheel className="h-4 w-4" />
+                        {wedding.theme}
+                      </div>
                     </div>
                   </div>
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button variant="outline">
+                        <Trash />
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent>
+                      <DialogHeader>
+                        <DialogTitle>Delete wedding</DialogTitle>
+                        <DialogDescription>
+                          All information related to this wedding will be
+                          permanently deleted.
+                        </DialogDescription>
+                        <div className="flex flex-row gap-2 mt-2">
+                          <Button variant="destructive">Delete wedding</Button>
+                          <Button variant="outline">Cancel</Button>
+                        </div>
+                      </DialogHeader>
+                    </DialogContent>
+                  </Dialog>
                 </div>
-                <div className="flex gap-2 w-full md:w-auto">
+                <div className="flex gap-2 w-full">
                   <Button
-                    className="flex-1 md:flex-none"
+                    className="flex-1"
                     variant="outline"
                     onClick={() => router.push(`/wedding/${wedding.id}/tasks`)}
                   >
@@ -86,7 +122,7 @@ export function WeddingList() {
                     Tasks
                   </Button>
                   <Button
-                    className="flex-1 md:flex-none"
+                    className="flex-1"
                     onClick={() => router.push(`/wedding/${wedding.id}/plan`)}
                   >
                     <Sparkles className="h-4 w-4 mr-2" />
